93031ddf6371c5e512399c546cd2b859501eb52c
wincrazy <2381227927@qq.com>
2026-06-05 17:19:25 +0800

    feat: 新增ArrayUtil工具类(StringBuilder版) — 用StringBuilder实现数组转字符串，格式[1, 2, 3]

---
+++ src/main/java/com/常见API/apistringapidemo7/ArrayUtil.java
@@ -0,0 +1,19 @@
+package com.常见API.apistringapidemo7;
+
+public class ArrayUtil {
+
+    public static String arrayToString2(int[] arr) {
+        //创建
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                sb.append(arr[i]);
+                sb.append("]");
+            } else {
+                sb.append(arr[i]);
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+}
