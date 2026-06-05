6b0e6e582d91ebcd11e47fe5f19575051366a4e5
wincrazy <2381227927@qq.com>
2026-06-05 17:18:28 +0800

    feat: 新增字符统计 — 遍历统计字符串中大写字母、小写字母、数字的个数

---
+++ src/main/java/com/常见API/apistringapidemo3/Test2.java
@@ -0,0 +1,35 @@
+package com.常见API.apistringapidemo3;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("请输入字符串");
+            String str = sc.next();
+
+            //遍历字符串中得到的每一个字符
+            int upperCount =0;
+            int lowerCount =0;
+            int numberCount =0;
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c >= 'a' && c <= 'z') {
+                    lowerCount++;
+                } else if (c >= 'A' && c <= 'Z') {
+                    upperCount++;
+                } else if (c >= '0' && c <= '9') {
+                    numberCount++;
+                } else {
+                    System.out.println("其他");
+                }
+            }
+            System.out.println("大写字符" + upperCount);
+            System.out.println("小写字符" + lowerCount);
+            System.out.println("数字字符" + numberCount);
+
+        }
+
+    }
+}
