ddcfb44c492e3030b1872e484e1b3f18f69f4b37
wincrazy <2381227927@qq.com>
2026-06-05 17:18:44 +0800

    feat: 新增字符串截取练习 — substring截取，用户名首字保留其余***替代

---
+++ src/main/java/com/常见API/apistringapidemo3/Test4.java
@@ -0,0 +1,19 @@
+package com.常见API.apistringapidemo3;
+
+public class Test4 {
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        String substr = str.substring(1, 3);
+        String substring = str.substring(1);
+        System.out.println(substr);
+        System.out.println(substring);
+
+
+        //只保留用户名的第一个字符，后面用三个***替代
+        String username="zhangsan";
+        CharSequence subSequence = username.subSequence(0, 1);
+        System.out.println("用户名："+subSequence+"***");
+
+    }
+
+}
