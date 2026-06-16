67334a747c45a3b1df3c19de15e6d0a4c9341c59
wincrazy <2381227927@qq.com>
2026-06-16 09:15:49 +0800

    1111

--- src/main/java/com/常见API/Integer/Test3.java
+++ src/main/java/com/常见API/Integer/Test3.java
@@ -2,4 +2,13 @@ package com.常见API.Integer;
 
 public class Test3 {
 
+    public static void main(String[] args) {
+        int i1 = 100;
+        Integer i2 = 200;// 触发自动装箱
+        //当基本数据类型i1+包装类i2，他们在相加的时候，就出发了自动拆箱
+        //触发自动装箱
+        Integer i3 = i1 + i2;
+        System.out.println(i3);
+    }
+
 }
